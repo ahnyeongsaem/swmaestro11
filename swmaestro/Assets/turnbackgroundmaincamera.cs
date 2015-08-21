e163146e86fe3a41c2040e1ab6d58bec1be8dd7a
ahnyeongsaem <ahnsaem90@gmail.com>
2015-08-22 06:24:46 +0900

    2015/08/22 메뉴랑 연동까지

---
+++ swmaestro/Assets/turnbackgroundmaincamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnbackgroundmaincamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, Time.deltaTime * 2f, 0);
+	}
+}
