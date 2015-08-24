4e0aea4863a4da30667e9f6f500109b09f7e6406
ahnyeongsaem <ahnsaem90@gmail.com>
2015-08-24 19:26:33 +0900

    2015/08/25 mamuri
    ignore resetup and asset add and stage add
    and bug fix(not finish)
    and main manu

---
+++ swmaestro/Assets/mainmenubutton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainmenubutton : MonoBehaviour {
+
+	public void gamestart()
+	{
+		Application.LoadLevel ("test2");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
